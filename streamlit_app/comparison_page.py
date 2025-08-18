51612cc171ec8d4155a930332db247d2308cb2ff
sergejlembke <halloballo@riseup.net>
2025-08-18 22:54:35 +0200

    removal, as it's now included in model_overview_page

--- streamlit_app/comparison_page.py
+++
@@ -1,6 +0,0 @@
-# --- Third-party imports ---
-import streamlit as st
-
-def app() -> None:
-    st.title("Comparison of Face Recognition Models")
-    st.write("This page compares the performance of different face recognition models.")
